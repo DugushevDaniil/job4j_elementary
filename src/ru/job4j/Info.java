0125c6839817ee0de36cdc7271845e3a6be4083f
DugushevDaniil <danillolec@gmail.com>
2021-03-12 11:25:10 +0300

    3. Арифметические операции. [#492037]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("10.03.2021");
+        char x = 'm';
+        System.out.println(x);
     }
 }
