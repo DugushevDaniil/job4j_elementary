abbf9c92cf2a46fd69b1ea70bce53521fda6d2d4
DugushevDaniil <danillolec@gmail.com>
2021-06-15 14:16:45 +0300

    3. Тесты для определения расстояния между точками. [#159459]

---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+    @Test
+    public void when00to20then2() {
+        int expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when02to00then2() {
+        int expected = 2;
+        int x1 = 0;
+        int y1 = 2;
+        int x2 = 0;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when03to00then3() {
+        int expected = 3;
+        int x1 = 0;
+        int y1 = 3;
+        int x2 = 0;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when03to03then6() {
+        int expected = 6;
+        int x1 = 0;
+        int y1 = 3;
+        int x2 = 0;
+        int y2 = -3;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when11to57then721() {
+        double expected = 7.21;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 5;
+        int y2 = 7;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
