6db956508da1afded8515a6e6f14d0338a2f191b
DugushevDaniil <danillolec@gmail.com>
2021-06-26 12:43:11 +0300

    5.7. Ипотека [#156315 #171565]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         percent /= 100;
-        while (amount >= 0) {
+        while (amount > 0) {
             amount = amount + percent * amount;
             amount -= salary;
             year += 1;
